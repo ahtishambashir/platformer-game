b8cdcc74d5492e65b5fbfffc7c1b6e0bc0c6d758
Ahtishm Bashir <ahtishamdoggar@gmail.com>
2025-11-13 04:55:08 +0500

    Modified the keys object

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,14 +58,28 @@ class Player {
 
 const player = new Player();
 
+
 const animate = () => {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   player.update();
+
+
+  if (keys.rightKey.pressed && player.position.x < proportionalSize(400)) {
+    player.velocity.x = 5;
+  }
+
 }
 
-const keys = {}
 
+const keys = {
+  rightKey: {
+    pressed: false
+  },
+  leftKey: {
+    pressed: false
+  }
+};
 
 const startGame = () => {
   canvas.style.display = "block";
