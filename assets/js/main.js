323ef2c727134f990650209dbbdf1dc255a8f75d
Ahtishm Bashir <ahtishamdoggar@gmail.com>
2025-11-12 23:42:33 +0500

    Added the updated and draw

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,11 +31,26 @@ class Player {
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 
-
   update() {
     this.draw();
     this.position.x += this.velocity.x;
-  }
+    this.position.y += this.velocity.y;
 
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      if (this.position.y < 0) {
+        this.position.y = 0;
+        this.velocity.y = gravity;
+      }
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
+
+    if (this.player.x < width) {
+
+    }
+
+
+  }
 
 }
