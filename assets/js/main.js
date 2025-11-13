a4e58e095620acdc95264a00655af0d87a14964d
Ahtishm Bashir <ahtishamdoggar@gmail.com>
2025-11-13 21:56:56 +0500

    added the switch statement

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,6 +91,27 @@ const movePlayer = (key, xVelocity, isPressed) => {
   }
 
 
+  switch (key) {
+    case "ArrowLeft":
+      keys.leftKey.pressed = isPressed;
+      if (xVelocity === 0) {
+        player.velocity.x = xVelocity;
+      }
+      player.velocity.x -= xVelocity;
+      break;
+    case "ArrowUp":
+    case " ":
+    case "Spacebar":
+      player.velocity.y -= 8;
+      break;
+    case "ArrowRight":
+      keys.rightKey.pressed = isPressed
+      if (xVelocity === 0) {
+        player.velocity.x = xVelocity;
+      }
+      player.velocity.x += xVelocity;
+      break;
+  }
 
 }
 
